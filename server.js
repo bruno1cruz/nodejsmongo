bd9095b5ded3e0ed7d60c2f6763d4180209ece1e
OpenShift guest <cruzbruno2000@yahoo.com.br>
2014-07-17 23:32:00 -0400

    configurção dporta e ip

--- server.js
+++ server.js
@@ -35,7 +35,10 @@ load('models')
 load('sockets')
 .into(io);
 
-server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, function(){
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+server.listen(port,ipaddress, function(){
   console.log("Megatip Ofertas - Online");
 });
 
