f3231428ce22f6b97152ea9e1650545993c9741a
OpenShift guest <cruzbruno2000@yahoo.com.br>
2014-07-23 23:01:15 -0400

    logging

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,11 @@
 exports.notFound = function(req, res, next) {
-  res.status(404);
-  res.render('not-found');
+    console.error("## Erro inesperado: " + error);
+    res.status(404);
+    res.render('not-found');
 };
 
 exports.serverError = function(error, req, res, next) {
-    console.log("## Erro inesperado: " + error);
-  res.status(500);
-  res.render('server-error', {error: error});
+    console.error("## Erro inesperado: " + error);
+    res.status(500);
+    res.render('server-error', {error: error});
 };
